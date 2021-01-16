b493a036ed756b716a3bd4fc76d321e376232184
ORodriguezV <ORodriguezV@users.noreply.github.com>
2021-01-15 21:53:09 -0600

    Not useful anymore

--- BlazorToDoDemo.Database/dbo/Tables/Status.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[Status] (
-    [idStatus] UNIQUEIDENTIFIER NOT NULL,
-    [status]   NVARCHAR (100)   NOT NULL,
-    CONSTRAINT [PK_Status] PRIMARY KEY CLUSTERED ([idStatus] ASC)
-);
-
